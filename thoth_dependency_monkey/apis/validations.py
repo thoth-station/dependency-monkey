9ee8928929f8dde1a0b7a3f5811caacddb3c3e18
Christoph Görn <goern@redhat.com>
2018-02-25 16:52:13 +0100

    added copyright notice to some files

--- thoth_dependency_monkey/apis/validations.py
+++ thoth_dependency_monkey/apis/validations.py
@@ -1,4 +1,22 @@
-import logging
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#   thoth-dependency-monkey
+#   Copyright(C) 2018 Christoph Görn
+#
+#   This program is free software: you can redistribute it and / or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Thoth: Dependency Monkey API"""
 
 from werkzeug.exceptions import BadRequest, ServiceUnavailable
 from flask import request
@@ -19,7 +37,6 @@ validation = ns.model('Validation', {
 
 
 DAO = ValidationDAO()
-logger = logging.getLogger(__file__)
 
 
 @ns.route('/<string:id>')
